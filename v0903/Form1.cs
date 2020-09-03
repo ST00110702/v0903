d98a4ea01412140c84122283d9851cc63a383526
ST0011 <st.game.program@gmail.com>
2020-09-03 09:48:34 +0900

    復習

--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -11,10 +11,33 @@ using System.Windows.Forms;
 namespace v0903
 {
     public partial class Form1 : Form
-    {
+    { 
+        
+        int vx =rand.Next(-20, 21);
+        int vy=rand.Next(-20,21);
+        int point = 100;
+        static Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            label1.Left=rand.Next(ClientSize.Width-label1.Size.Width);
+            label1.Top=rand.Next(ClientSize.Height-label1.Size.Height);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Top = vx;
+            label1.Left = vx;
+
+            if (label1.Left <= 0)
+            {
+                vx 
+            }
+
+
+            
         }
     }
 }
