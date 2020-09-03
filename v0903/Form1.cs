b864499ebd7ba5107a95c0581275946d393fa1e8
ST0011 <st.game.program@gmail.com>
2020-09-03 12:14:44 +0900

    演習

--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -12,40 +12,87 @@ namespace v0903
 {
     public partial class Form1 : Form
     { 
-        
-        int vx =rand.Next(-20, 21);
-        int vy=rand.Next(-20,21);
-        int point = 100;
-        static Random rand = new Random();
+        static Random rand =new Random();
+        int[] vx = new int[3];
+        int []vy=new int[3];
+        Label[] Labels = new Label[100];
 
         public Form1()
         {
             InitializeComponent();
-            label1.Left=rand.Next(ClientSize.Width-label1.Size.Width);
-            label1.Top=rand.Next(ClientSize.Height-label1.Size.Height);
-
+            for (int i = 0; i < 3;i++ )
+            {
+                vx[i] = rand.Next(-20, 21);
+                vy[i] = rand.Next(-20, 21);
+                Labels[i] = new Label();
+                Labels[i].AutoSize = true;
+                Labels[i].Text = "★";
+                Controls.Add(Labels[i]);
+            }
+          
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Top += vy;
-            label1.Left += vx;
+
+            label1.Top += vy[0];
+            label1.Left += vx[0];
+            label2.Top += vy[1];
+            label2.Left += vx[1];
+            label3.Top += vy[2];
+            label3.Left += vx[2];
 
             if (label1.Left < 0)
             {
-                vx = Math.Abs(vx);
+                vx[0] = Math.Abs(vx[0]);
             }
             if (label1.Top < 0)
             {
-                vy = Math.Abs(vy);
+                vy[0] = Math.Abs(vy[0]);
             }
             if (label1.Right > ClientSize.Width)
             {
-                vx = -Math.Abs(vx);
+                vx[0] = -Math.Abs(vx[0]);
             }
             if (label1.Bottom > ClientSize.Height) 
             {
-                vy = -Math.Abs(vy);
+                vy[0] = -Math.Abs(vy[0]);
+            }
+
+
+            if (label2.Left < 0)
+            {
+                vx[1] = Math.Abs(vx[1]);
+            }
+            if (label2.Top < 0)
+            {
+                vy[1] = Math.Abs(vy[1]);
+            }
+            if (label2.Right > ClientSize.Width)
+            {
+                vx[1] = -Math.Abs(vx[1]);
+            }
+            if (label2.Bottom > ClientSize.Height)
+            {
+                vy[1] = -Math.Abs(vy[1]);
+            }
+
+
+            if (label3.Left < 0)
+            {
+                vx[2] = Math.Abs(vx[2]);
+            }
+            if (label3.Top < 0)
+            {
+                vy[2] = Math.Abs(vy[2]);
+            }
+            if (label3.Right > ClientSize.Width)
+            {
+                vx[2] = -Math.Abs(vx[2]);
+            }
+            if (label3.Bottom > ClientSize.Height)
+            {
+                vy[2] = -Math.Abs(vy[2]);
             }
 
             Point mp = PointToClient(MousePosition);
@@ -55,9 +102,6 @@ namespace v0903
                 && (mp.Y > label1.Bottom)
                 )
                 timer1.Enabled = false;
- 
-
-            
         }
     }
 }
