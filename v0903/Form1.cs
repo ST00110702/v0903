d681097d07304c30403c75bca2e1d6cf3e48a516
ST0011 <st.game.program@gmail.com>
2020-09-03 10:19:03 +0900

    復習

--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -31,23 +31,31 @@ namespace v0903
             label1.Top += vy;
             label1.Left += vx;
 
-            if (label1.Left <= 0)
+            if (label1.Left < 0)
             {
                 vx = Math.Abs(vx);
             }
-            if (label1.Top <= 0)
+            if (label1.Top < 0)
             {
                 vy = Math.Abs(vy);
             }
-            if (label1.Right >= ClientSize.Width)
+            if (label1.Right > ClientSize.Width)
             {
                 vx = -Math.Abs(vx);
             }
-            if (label1.Bottom >= ClientSize.Height) 
+            if (label1.Bottom > ClientSize.Height) 
             {
                 vy = -Math.Abs(vy);
             }
 
+            Point mp = PointToClient(MousePosition);
+            if ((mp.X >= label1.Left)
+                && (mp.X < label1.Right)
+                && (mp.Y >= label1.Top)
+                && (mp.Y > label1.Bottom)
+                )
+                timer1.Enabled = false;
+ 
 
             
         }
